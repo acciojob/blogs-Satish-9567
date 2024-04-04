de063795147ace90613ffce7197350585277a52c
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2024-04-04 13:34:06 +0530

    Update ImageService.java

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,35 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
+        Image image=new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+        Blog blog=blogRepository2.findById(blogId).get();
+        image.setBlog(blog);
+        image =imageRepository2.save(image);
+        return image;
 
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        Image image = imageRepository2.findById(id).get();
+        int countImages = 0;
+        String[] screenSize = screenDimensions.split("X");
+        String[] imageSize = image.getDimensions().split("X");
 
+        // finding height and width of image
+        int imageH = Integer.parseInt(imageSize[0]);
+        int imageW = Integer.parseInt(imageSize[1]);
+
+        // finding height and width of screen
+        int screenH = Integer.parseInt(screenSize[0]);
+        int screenW = Integer.parseInt(screenSize[1]);
+
+        return (screenH/imageH)*(screenW/imageW);
     }
 }
