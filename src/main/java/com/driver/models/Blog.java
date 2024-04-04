1d07a3990f982cbc37ecbfa7997174f41eb8bd89
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2024-04-04 13:45:26 +0530

    Update Blog.java

--- src/main/java/com/driver/models/Blog.java
+++ src/main/java/com/driver/models/Blog.java
@@ -1,26 +1,46 @@
 package com.driver.models;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table
 public class Blog {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer blog_id;
+    private Integer id;
+
     private String title;
     private String content;
+    @CreationTimestamp
+    private Date pubDate;
 
     @ManyToOne
     @JoinColumn
     private User user;
 
-    public Integer getBlog_id() {
-        return blog_id;
+    @OneToMany(mappedBy = "blog", cascade = CascadeType.ALL)
+    private List<Image> imageList = new ArrayList<>();
+    public Blog() {
     }
 
-    public void setBlog_id(Integer blog_id) {
-        this.blog_id = blog_id;
+    public Blog(String title, String content, Date pubDate, User user) {
+        this.title = title;
+        this.content = content;
+        this.pubDate = pubDate;
+        this.user = user;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -39,11 +59,27 @@ public class Blog {
         this.content = content;
     }
 
+    public Date getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(Date pubDate) {
+        this.pubDate = pubDate;
+    }
+
     public User getUser() {
         return user;
     }
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public List<Image> getImageList() {
+        return imageList;
+    }
+
+    public void setImageList(List<Image> imageList) {
+        this.imageList = imageList;
     }
 }
