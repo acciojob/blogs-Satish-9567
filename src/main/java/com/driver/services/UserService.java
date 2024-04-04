73c2f2cea3da8fad5b4ab8855bdfb637850cefca
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2024-04-04 13:35:45 +0530

    Update UserService.java

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -1,3 +1,31 @@
+// package com.driver.services;
+
+// import com.driver.models.*;
+// import com.driver.repositories.UserRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// @Service
+// public class UserService {
+//     @Autowired
+//     UserRepository userRepository3;
+
+//     public User createUser(String username, String password){
+
+
+//     }
+
+//     public void deleteUser(int userId){
+
+//     }
+
+//     public User updateUser(Integer id, String password){
+
+//     }
+// }
 package com.driver.services;
 
 import com.driver.models.*;
@@ -11,18 +39,25 @@ import java.util.List;
 @Service
 public class UserService {
     @Autowired
-    UserRepository userRepository3;
+    UserRepository userRepository;
 
     public User createUser(String username, String password){
-
-
+        User user=new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user=userRepository.save(user);
+        return user;
     }
 
     public void deleteUser(int userId){
-
+        userRepository.deleteById(userId);
     }
 
     public User updateUser(Integer id, String password){
-
+        User user=userRepository.findById(id).get();
+        user.setPassword(password);
+        user=userRepository.save(user);
+        return user;
     }
 }
+
