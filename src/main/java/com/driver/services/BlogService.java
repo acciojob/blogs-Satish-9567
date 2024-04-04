1dd8ca57a692bcc3c9e055eacc5b7e78819d51af
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2024-04-04 13:47:52 +0530

    Update BlogService.java

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -1,43 +1,18 @@
-// package com.driver.services;
-
-// import com.driver.models.Blog;
-// import com.driver.models.Image;
-// import com.driver.models.User;
-// import com.driver.repositories.BlogRepository;
-// import com.driver.repositories.ImageRepository;
-// import com.driver.repositories.UserRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-
-// import java.util.ArrayList;
-// import java.util.Date;
-// import java.util.List;
-
-// @Service
-// public class BlogService {
-//     @Autowired
-//     BlogRepository blogRepository1;
-
-//     @Autowired
-//     UserRepository userRepository1;
-
-//     public Blog createAndReturnBlog(Integer userId, String title, String content) {
-//         //create a blog at the current time
-
-//     }
-
-//     public void deleteBlog(int blogId){
-//         //delete blog and corresponding images
-
-//     }
-// }
 package com.driver.services;
 
 import com.driver.models.Blog;
+import com.driver.models.Image;
+import com.driver.models.User;
 import com.driver.repositories.BlogRepository;
+import com.driver.repositories.ImageRepository;
 import com.driver.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.StyledEditorKit;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class BlogService {
@@ -49,18 +24,21 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
+        User user=userRepository1.findById(userId).get();
         Blog blog=new Blog();
-        blog.setBlog_id(userId);
         blog.setTitle(title);
+        blog.setUser(user);
+        blog.setPubDate(new Date());
         blog.setContent(content);
-        blog= blogRepository1.save(blog);
+        user.getBlogList().add(blog);
+        blogRepository1.save(blog);
+        userRepository1.save(user);
         return blog;
     }
-
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
         blogRepository1.deleteById(blogId);
-
     }
-}
 
+    /* <----------------------------------DONE-------------------------------------->*/
+}
