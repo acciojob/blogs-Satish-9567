7a94b67b82e1e602c59514a402d28640c1cc1c07
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2024-04-04 13:37:21 +0530

    Update BlogController.java

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -13,10 +13,28 @@ import java.util.List;
 @RequestMapping("/blogs")
 public class BlogController {
 
+    // @PostMapping
+    // public ResponseEntity createBlog(@RequestParam Integer userId ,
+    //                                  @RequestParam String title,
+    //                                  @RequestParam String content) {
+    //     // Create a blog and add it under given user
+    //     return new ResponseEntity<>(HttpStatus.CREATED);
+    // }
+
+    // @DeleteMapping("/{blogId}")
+    // public ResponseEntity<Void> deleteBlog(@PathVariable int blogId) {
+    //     // Delete the blog using deleteById
+    //     return new ResponseEntity<>(HttpStatus.OK);
+    // }
+     @Autowired
+    BlogService blogService;
+
     @PostMapping
     public ResponseEntity createBlog(@RequestParam Integer userId ,
                                      @RequestParam String title,
                                      @RequestParam String content) {
+        blogService.createAndReturnBlog(userId,title,content);
+
         // Create a blog and add it under given user
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
@@ -24,6 +42,7 @@ public class BlogController {
     @DeleteMapping("/{blogId}")
     public ResponseEntity<Void> deleteBlog(@PathVariable int blogId) {
         // Delete the blog using deleteById
+        blogService.deleteBlog(blogId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
